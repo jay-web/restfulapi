95db322beb3c9903eb3770af9599f2e300447fcb
jay-web <jay.developer12@gmail.com>
2020-07-07 17:16:53 +0530

    added POST request to add new wizard

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ const express = require("express");
 
 
 const app = express();
+
 const port = 3000;
 
+app.use(express.json());            // middleware to parse req.body
+
+// ? ==============================
 // * Read the data from file
 const wizards = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/db.json`));
 
 
+// ? ==============================
+
+// * GET METHOD / root , to fetch all the data
 app.get("/api/v1/wizards", (req, res) => {
 
     res
@@ -22,6 +29,31 @@ app.get("/api/v1/wizards", (req, res) => {
         });
     
 });
+
+// ? =================================
+
+app.post("/api/v1/wizards", (req, res) => {
+
+    // const newId = wizards[wizards.length - 1] + 1;
+    // const newWizard = Object.assign({id: newId}, req.body);
+
+    console.log("body", req.body);
+
+    wizards.push(req.body);         
+
+    // ? write the udpated list of wizards in file
+
+    fs.writeFile(`${__dirname}/dev-data/data/db.json`, JSON.stringify(wizards), () => {
+        res.status(200).json({
+            status: "success",
+            data: {
+                newMember: req.body
+            }
+        });
+    });
+
+});
+
 
 
 app.listen(port, () => {
